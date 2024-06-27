6865abfbae1f8287d528c5906c7d6d04207341c6
miqhi <92723627+miqhi@users.noreply.github.com>
2024-06-27 20:31:39 +0200

    big updates happended
    + better enemy movement ai, shit plays you like a damn fiddle
    + healtbars for enemies changed
    + added knockback once damage is taken (only enemies)

---
+++ AnimeFightSimulator/Assets/Scripts/Character/PlayerStats.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int knockbackForce = 50; // Default knockback force
+
+    // You can add more player-related stats here
+}
